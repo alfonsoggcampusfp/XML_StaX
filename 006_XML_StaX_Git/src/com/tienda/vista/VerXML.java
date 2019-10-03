18152ac12751e9ee1e0634909071414f05f2a8a9
alfonsoggcampusfp <49977608+alfonsoggcampusfp@users.noreply.github.com>
2019-10-03 11:51:30 +0200

    Modifico ruta archivo XML
    Modifico ruta archivo XML

--- 006_XML_StaX_Git/src/com/tienda/vista/VerXML.java
+++ 006_XML_StaX_Git/src/com/tienda/vista/VerXML.java
@@ -11,7 +11,7 @@ public class VerXML {
 		// TODO Auto-generated method stub
 
 		ParsearXML read = new ParsearXML();
-        List<Producto> readConfig = read.readConfig("C:\\alfonso\\Clientes.xml");
+        List<Producto> readConfig = read.readConfig("..\\..\\..\\..\\Clientes.xml");
         for (Producto item : readConfig) {
             System.out.println(item);
 		
